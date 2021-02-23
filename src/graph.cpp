082ee5c68eead72ebe9a16fddfc87522ad2da4fd
cecilia <lsscecilia8888@gmail.com>
2021-02-23 14:05:58 +0800

    grap model - to be revised

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <vector> 
-using namespace std;
-
-template <class T>
-class Graph{
-  private:
-    struct{
-      pair<T,T> edge; 
-      int weight; 
-    }; 
-    int numVertices; 
-    vector<T> vertices;  
-}; 
+#include "graph.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string> 
+
+#include <config.h>
+#include "graph.h"
+
+using namespace std; 
 
 int main(int argc, char * argv[])
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "creating graph ..." << endl; 
+    //test(); 
+    
+    Graph<std::string> graph; 
+    graph.addVertice("s1"); 
+    graph.addVertice("s2"); 
+    graph.addVertice("s3"); 
+    graph.addVertice("s4"); 
+    graph.addVertice("s5"); 
+    graph.addVertice("s6"); 
+    graph.addEdge("s1", "s4", 10); 
+    graph.addEdge("s2", "s5", 10); 
+    graph.addEdge("s3", "s6", 10); 
+    cout << "num vertices:" << graph.getNumVertices() << endl; 
+    cout << "num edges:" << graph.getNumEdges()  << endl; 
     return 0;
 }
