84e0076774694f8239da2fabf112206dd437af42
cecilia <lsscecilia8888@gmail.com>
2021-02-24 14:15:55 +0800

    unit test for math vector

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,42 @@
 #include <string> 
 
 #include <config.h>
-#include "graph.h"
+#include "algorithm.h"
 
 using namespace std; 
 
 int main(int argc, char * argv[])
 {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "creating graph ..." << endl; 
-    //test(); 
+    vector<Vertex> vertices; 
+    vertices.push_back({{4,1}, {0,0}});
+    vertices.push_back({{4.4,1}, {0,0}});
+    vertices.push_back({{3.2,1}, {0,0}});
+    vertices.push_back({{3.3,1}, {0,0}});
+    vertices.push_back({{1.5,1}, {0,0}});
     
-    Graph<std::string> graph; 
+    vector<vector<bool>> edges; 
+    edges.push_back({false,true,true,true, true}); 
+    edges.push_back({true,false,true,true, true}); 
+    edges.push_back({true,true,false,true, true}); 
+    edges.push_back({true,true,true,false, true}); 
+    edges.push_back({true,true,true,true, false}); 
+
+    for (int i=0;i<vertices.size(); i++){
+        cout << i << "| pos(" << vertices[i].pos.x << " ," << vertices[i].pos.y << "), disp("
+        << vertices[i].disp.x << " ," << vertices[i].disp.y << ")" << endl; 
+    }
+    
+    directedForceAlgorithm(vertices, edges, 5,5,100); 
+
+    for (int i=0;i<vertices.size(); i++){
+        cout << i << "| pos(" << vertices[i].pos.x << " ," << vertices[i].pos.y << "), disp("
+        << vertices[i].disp.x << " ," << vertices[i].disp.y << ")" << endl; 
+    }
+    /*
+    std::cout << "creating graph ..." << endl; 
+    
+    graph::Graph<std::string> graph; 
     graph.addVertice("s1"); 
     graph.addVertice("s2"); 
     graph.addVertice("s3"); 
@@ -23,6 +48,6 @@ int main(int argc, char * argv[])
     graph.addEdge("s2", "s5", 10); 
     graph.addEdge("s3", "s6", 10); 
     cout << "num vertices:" << graph.getNumVertices() << endl; 
-    cout << "num edges:" << graph.getNumEdges()  << endl; 
+    cout << "num edges:" << graph.getNumEdges()  << endl; */
     return 0;
 }
--- test/graph_visualisation_test.cpp
+++ test/graph_visualisation_test.cpp
@@ -1,5 +1,82 @@
 #include "gtest/gtest.h"
 
+#include "../src/mathVector.h"
+
+TEST (MathVectorTest, additionOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,4); 
+    MathVector mv = mv1 + mv2; 
+    EXPECT_EQ(4, mv.x); 
+    EXPECT_EQ(6, mv.y); 
+}
+
+TEST (MathVectorTest, minusOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,2); 
+    MathVector mv = mv1 - mv2; 
+    EXPECT_EQ(-2, mv.x); 
+    EXPECT_EQ(0, mv.y); 
+}
+
+TEST (MathVectorTest, multiplyOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,4); 
+    MathVector mv = mv1 * mv2; 
+    EXPECT_EQ(3, mv.x); 
+    EXPECT_EQ(8, mv.y); 
+}
+
+TEST (MathVectorTest, multiplyByDouble){
+    MathVector mv1(1,2); 
+    MathVector mv = mv1*5; 
+    EXPECT_EQ(5, mv.x); 
+    EXPECT_EQ(10,mv.y); 
+}
+
+TEST (MathVectorTest, divisionOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,4); 
+    MathVector mv = mv1/mv2; 
+    EXPECT_EQ((double)1/3, mv.x); 
+    EXPECT_EQ(0.5,mv.y); 
+}
+
+TEST (MathVectorTest, divideByDouble){
+    MathVector mv1(6,2); 
+    MathVector mv = mv1/2; 
+    EXPECT_EQ(3, mv.x); 
+    EXPECT_EQ(1,mv.y); 
+}
+
+TEST (MathVectorTest, plusEqualOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,4); 
+    mv1 += mv2; 
+    EXPECT_EQ(4, mv1.x); 
+    EXPECT_EQ(6, mv1.y); 
+}
+
+TEST (MathVectorTest, minusEqualOperator){
+    MathVector mv1(1,2); 
+    MathVector mv2(3,4); 
+    mv1 -= mv2; 
+    EXPECT_EQ(-2, mv1.x); 
+    EXPECT_EQ(-2, mv1.y); 
+}
+
+TEST (MathVectorTest, abs){
+    MathVector mv1(1,2); 
+    double abs = mv1.abs();
+    EXPECT_EQ(sqrt(5), abs); 
+}
+
+TEST (MathVectorTest, min){
+    MathVector mv1(10,2);
+    MathVector mv = mv1.min(5); 
+    EXPECT_EQ(5, mv.x); 
+    EXPECT_EQ(2, mv.y);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
