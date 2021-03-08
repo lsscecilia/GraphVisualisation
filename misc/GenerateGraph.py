432df9fa7c12a5729705fecfd7de603abfdc9d91
lsscecilia <lsscecilia8888@gmail.com>
2021-03-09 01:13:04 +0800

    generate circular graph in python script

--- misc/GenerateGraph.py
+++ misc/GenerateGraph.py
@@ -14,17 +14,40 @@ def generate(n):
   print("number of edges: ", numEdges)
 
 
+def generateCircularGraph(n):
+  filename = str(n) + "n_c.txt"
+  f= open(filename,"w+")
+  numEdges=0
+  for i in range(n-1):
+    line = str(i) + "--" + str(i+1) +"\n"
+    f.write(line)
+    numEdges+=1
+  line = str(0) + "--" + str(n-1) +"\n"
+  f.write(line)
+  numEdges+=1
+  f.close()
+  print('number of nodes: ', n)
+  print("number of edges: ", numEdges)
+
+
 if __name__ == "__main__":
   arguments = len(sys.argv)-1
-  longOptions =['version', 'help','incomplete', 'num']
-  options = "vhin:"
+  longOptions =['version', 'help','incomplete', 'circular','num']
+  options = "vhicn:"
   loop = False
+  circular = False
   numNodes = 10 #default number
   try: 
     opts, args = getopt.getopt(sys.argv[1:], options, longOptions)
   except getopt.GetoptError:
     print('wrong params')
   for opt,arg in opts:
+    if opt in ("-c", "--circular"):
+      circular = True
     if opt in ("-n", "--num"):
       numNodes = int(arg)
-  generate(numNodes)
+  
+  if circular:
+    generateCircularGraph(numNodes)
+  else:
+    generate(numNodes)
